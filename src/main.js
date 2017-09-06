fe09e5408da39faebaf81b20aee648d99dd81440
chfw <wangc_2011@hotmail.com>
2017-09-06 06:45:34 +0100

    :art: code refactoring for testing

--- src/main.js
+++ src/main.js
@@ -4,22 +4,26 @@ var program = require('commander');
 const scrappeteer = require('./scrappeteer');
 const chalk = require('chalk');
 
-
 program
 	.arguments('<url/file>')
 	.option('-f, --format <png/jpeg>', 'image format')
 	.option('-o, --output <outputname>', 'output file name')
 	.action(function(url_or_file){
-		if (typeof program.output === 'undefined'){
-			program.output = 'output';
-		}
-		if (typeof program.format === 'undefined'){
-			program.format = 'png';
-		} else if (program.format != 'png' || program.format != 'jpeg'){
-			console.error(chalk.cyan("Unsupported file format : ") +
-						  chalk.bold.red(program.format));
-			process.exit(1);
-		}
-		scrappeteer(url_or_file, program.format, program.output);
+		main(url_or_file, program.format, program.output);
 	})
 	.parse(process.argv);
+
+
+function main(url_or_file, format, output){
+	if (typeof output === 'undefined'){
+		output = 'output';
+	}
+	if (typeof format === 'undefined'){
+		format = 'png';
+	} else if (format != 'png' || format != 'jpeg'){
+		console.error(chalk.cyan("Unsupported file format : ") +
+					  chalk.bold.red(format));
+		process.exit(1);
+	}
+	scrappeteer(url_or_file, format, output);
+}
